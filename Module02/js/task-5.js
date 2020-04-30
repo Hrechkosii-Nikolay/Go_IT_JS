43a438fa921784a7df07032f8967a3038e6f070d
Hrechkosii-Nikolay <60157425+Hrechkosii-Nikolay@users.noreply.github.com>
2020-04-30 16:13:27 +0300

    task5

--- Module02/js/task-5.js
+++ Module02/js/task-5.js
@@ -1,7 +1,17 @@
+// const checkForSpam = function (message) {
+//   message = message.toLowerCase();
+//   // const checkWords = ('sale', 'spam');
+//   return message.includes('sale') || message.includes('spam');
+// };
+
 const checkForSpam = function (message) {
   message = message.toLowerCase();
-  const checkWords = ('sale', 'spam');
-  return message + ' // ' + message.includes(checkWords);
+  const checkWords = ['sale', 'spam'];
+  let result = false;
+  for (let words of checkWords) {
+    if (message.includes(words)) result = true;
+  }
+  return message + ' // ' + result;
 };
 
 console.log(checkForSpam('Latest technology news')); // false
