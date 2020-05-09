754cfa1e0e9d6f8935aa340df7311f0e4f20730e
Hrechkosii-Nikolay <60157425+Hrechkosii-Nikolay@users.noreply.github.com>
2020-05-09 23:38:14 +0300

    add task 3

--- Module03/js/task-3.js
+++ Module03/js/task-3.js
@@ -1,15 +1,8 @@
 const findBestEmployee = function (employees) {
-  // let result = 0;
-  for (let employ of employees) {
-    console.log(employ);
-  }
-
-  // return
+  const maxValue = Math.max.apply(null, Object.values(employees));
+  return Object.keys(employees).filter(k => employees[k] === maxValue);
 };
 
-/*
- * Вызовы функции для проверки работоспособности твоей реализации.
- */
 console.log(
   findBestEmployee({
     ann: 29,
@@ -19,19 +12,19 @@ console.log(
   }),
 ); // lorence
 
-// console.log(
-//   findBestEmployee({
-//     poly: 12,
-//     mango: 17,
-//     ajax: 4,
-//   }),
-// ); // mango
+console.log(
+  findBestEmployee({
+    poly: 12,
+    mango: 17,
+    ajax: 4,
+  }),
+); // mango
 
-// console.log(
-//   findBestEmployee({
-//     lux: 147,
-//     david: 21,
-//     kiwi: 19,
-//     chelsy: 38,
-//   }),
-// ); // lux
+console.log(
+  findBestEmployee({
+    lux: 147,
+    david: 21,
+    kiwi: 19,
+    chelsy: 38,
+  }),
+); // lux
